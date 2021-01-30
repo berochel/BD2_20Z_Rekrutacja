81f91a4091193480e3bd7e4e9fc1721c6706bb84
Jaroslaw <zabuski.jaroslaw@gmail.com>
2021-01-30 20:16:34 +0100

    added Kierunek statistics.

--- app/src/com/company/AdminManageFrame.java
+++ app/src/com/company/AdminManageFrame.java
@@ -23,8 +23,14 @@ public class AdminManageFrame extends JFrame {
     private JLabel glownaLabel;
     private JLabel rezerwowaLabel;
     private JComboBox kierunkiComboBox;
+    private JLabel turaRozpLabel;
+    private JLabel turaZakLabel;
+    private JLabel kierunekAllLabel;
+    private JLabel kierunekGlownaLabel;
+    private JLabel kierunekRezerwowaLabel;
     List<String> rekrutacja_query;
     List<String> tura_query  = new ArrayList<>();
+    List<String> kierunek_query  = new ArrayList<>();
     List<Map<String, Object>> tura_set;
 
     public AdminManageFrame(String str) {
@@ -96,6 +102,10 @@ public class AdminManageFrame extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                kierunekGlownaLabel.setText(String.valueOf(0));
+                kierunekRezerwowaLabel.setText(String.valueOf(0));
+                kierunekAllLabel.setText(String.valueOf(0));
+
                 int number = turaComboBox.getSelectedIndex();
                 String temp = tura_query.get(number);
                 int tempIndexBeginning = temp.indexOf(".");
@@ -105,6 +115,12 @@ public class AdminManageFrame extends JFrame {
                 String query = "";
 
                 List<Map<String, Object>> tempList;
+
+                query = "SELECT data_zak, data_rozp from tura WHERE id_tury = '"+temp+"'";
+                tempList = Connect.connect(query);
+
+                turaRozpLabel.setText(String.valueOf(tempList.get(0).get("data_rozp")));
+                turaZakLabel.setText(String.valueOf(tempList.get(0).get("data_zak")));
 
                 query = "SELECT * from lista WHERE id_tury = '"+temp+"'";
                 tempList = Connect.connect(query);
@@ -128,9 +144,9 @@ public class AdminManageFrame extends JFrame {
                     String tempString = String.valueOf(map.get("rezerwowa"));
 
                     if(tempString.equals("N"))
-                        glownaCount = tempList2.size();
+                        glownaCount += tempList2.size();
                     else
-                        rezerwowaCount = tempList2.size();
+                        rezerwowaCount += tempList2.size();
 
                     kandydaci_set.addAll(tempList2);
 
@@ -142,19 +158,68 @@ public class AdminManageFrame extends JFrame {
                 query = "SELECT kierunek_studiow.nazwa, realizacja.kod from kierunek_studiow, realizacja WHERE kierunek_studiow.id_kierunku = realizacja.id_kierunku AND realizacja.id_kierunku IN (SELECT id_kierunku from realizacja WHERE kod IN (SELECT kod_realizacji from lista WHERE id_tury = '"+temp+"'))";
                 tempList = Connect.connect(query);
 
-                for (Map<String, Object> map : tempList) {
+                /*for (Map<String, Object> map : tempList) {
                     for (Map.Entry<String, Object> entry : map.entrySet()) {
                         System.out.println(entry.getKey() + " - " + entry.getValue());
                     }
-                }
+                }*/
 
-                List<String> kierunek_query  = new ArrayList<>();
+                kierunek_query.clear();
                 for (Map<String, Object> map : tempList) {
 
                     kierunek_query.add("Kierunek-"+map.get("kod")+", "+map.get("nazwa"));
                 }
 
                 kierunkiComboBox.setModel(new DefaultComboBoxModel<>(kierunek_query.toArray(new String[0])));
+            }
+        });
+        kierunkiComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String query = "";
+                List<Map<String, Object>> tempList;
+
+                int number = turaComboBox.getSelectedIndex();
+                String tempTura = tura_query.get(number);
+                int tempIndexBeginning = tempTura.indexOf(".");
+                if(tempIndexBeginning != -1) {
+                    tempTura = tempTura.substring(tempIndexBeginning+1);
+                }
+
+                number = kierunkiComboBox.getSelectedIndex();
+                String tempKierunek = kierunek_query.get(number);
+                tempIndexBeginning = tempKierunek.indexOf("-");
+                int tempIndexEnd = tempKierunek.indexOf(",");
+                if(tempIndexEnd != -1 && tempIndexBeginning != -1) {
+                    tempKierunek = tempKierunek.substring(tempIndexBeginning+1, tempIndexEnd);
+                }
+
+                query = "SELECT * from lista_kandydat WHERE id_listy IN (SELECT id_listy from lista WHERE kod_realizacji = '"+tempKierunek+"' AND id_tury = '"+tempTura+"')";
+                tempList = Connect.connect(query);
+
+
+                query = "SELECT * from lista_kandydat WHERE id_listy IN (SELECT id_listy from lista WHERE kod_realizacji = '"+tempKierunek+"' AND id_tury = '"+tempTura+"' AND rezerwowa = 'N')";
+                tempList = Connect.connect(query);
+                for (Map<String, Object> map : tempList) {
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+                        System.out.println(entry.getKey() + " - " + entry.getValue());
+                    }
+                }
+                int glownaNumber = tempList.size();
+
+
+                query = "SELECT * from lista_kandydat WHERE id_listy IN (SELECT id_listy from lista WHERE kod_realizacji = '"+tempKierunek+"' AND id_tury = '"+tempTura+"' AND rezerwowa = 'T')";
+                tempList = Connect.connect(query);
+                for (Map<String, Object> map : tempList) {
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+                        System.out.println(entry.getKey() + " - " + entry.getValue());
+                    }
+                }
+                int rezerwowaNumber = tempList.size();
+
+                kierunekGlownaLabel.setText(String.valueOf(glownaNumber));
+                kierunekRezerwowaLabel.setText(String.valueOf(rezerwowaNumber));
+                kierunekAllLabel.setText(String.valueOf(glownaNumber+rezerwowaNumber));
             }
         });
     }
